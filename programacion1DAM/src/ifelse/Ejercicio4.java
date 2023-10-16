f10e605a958763691e4950bf34ee9f2f3bec314d
Sierra <ivan.sierra.perez@colegiolitterator.com>
2023-10-16 17:36:36 +0200

    Ejercicio4ifelse perfeccionado

--- programacion1DAM/src/ifelse/Ejercicio4.java
+++ programacion1DAM/src/ifelse/Ejercicio4.java
@@ -1,5 +1,6 @@
 package ifelse;
 
+import java.time.Year;
 import java.util.Scanner;
 
 public class Ejercicio4 {
@@ -11,7 +12,9 @@ public class Ejercicio4 {
 		System.out.println("¿en que año naciste naciste?");
 		int nacimiento = sc.nextInt();
 		
-		int ano = 2023;
+		// Year.now () .get Value() es para marcar el año actual
+		
+		int ano = Year.now() .getValue();
 		
 		int resta= ano - nacimiento;
 		
