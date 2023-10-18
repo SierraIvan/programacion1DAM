94c281e30d4d4066982af415e9aed5125108446f
Sierra <ivan.sierra.perez@colegiolitterator.com>
2023-10-18 20:03:00 +0200

    Ejercicios for terminados

--- programacion1DAM/src/buclefor/Ejercicio4.java
+++ programacion1DAM/src/buclefor/Ejercicio4.java
@@ -7,19 +7,27 @@ public class Ejercicio4 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Introduce un numero entero");
+		System.out.println("Introduce un numero entero positivo");
 		
 		int n;
+		int f=1;
 		
 		Scanner sc = new Scanner(System.in);
 		n=sc.nextInt();
 		
-		if(n<0) {
+		if(n>0) {
 			
+			for(int i=1;i<=n;i++) {
+				
+				f=f * i;
+					System.out.println(f);
+			}
+			System.out.println("La multiplicacion de todos sus numero de este factorial es: " + f);
+		}else {
 			
+			System.out.println("Na, tu eres como rarito no?");
 		}
+		
 	}
-	
-	
-
 }
+
---
+++ programacion1DAM/src/buclefor/Ejercicio5.java
@@ -0,0 +1,23 @@
+package buclefor;
+
+import java.util.Scanner;
+
+public class Ejercicio5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("intraduzca un numero mayor que 1");
+		
+		int n =sc.nextInt();
+		for(int i=n;i>=1;i--)
+		
+		{
+			System.out.println(i);
+		}
+		
+	}
+
+}
