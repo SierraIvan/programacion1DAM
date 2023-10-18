8fb6f226cb54a9f60bfc7867692b7bda07c7309b
Sierra <ivan.sierra.perez@colegiolitterator.com>
2023-10-18 20:32:09 +0200

    Ejercicios while v1

---
+++ programacion1DAM/src/bulcewhile/Ejercicio2.java
@@ -0,0 +1,22 @@
+package bulcewhile;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		
+
+		int n=1;
+		int i=10;
+		
+		while(i>=n) {
+			System.out.println(i);
+			
+			i--;
+		}
+	}
+
+}
--- programacion1DAM/src/bulcewhile/Ejercicio3.java
+++ programacion1DAM/src/bulcewhile/Ejercicio3.java
@@ -2,25 +2,21 @@ package bulcewhile;
 
 import java.util.Scanner;
 
-public class Ejercicio1 {
+public class Ejercicio3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		
-		
-		
 		Scanner sc = new Scanner(System.in);
-		System.out.println("intraduzca un numero mayor que 1");
+		System.out.println("intraduzca un numero entero");
 		
 		int n =sc.nextInt();
+		int i=10;
 		
-		int i=1;
-		
-		while(i<=n) {
-			System.out.println(i);
+		while(i<n) {
+			System.out.println(n / 10);
 			
-			i++;
 		}
 	}
 
