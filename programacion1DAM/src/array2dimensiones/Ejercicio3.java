b71f074c96c9fe74d5eb4e4412dea8000d624c77
Sierra <ivan.sierra.perez@colegiolitterator.com>
2023-11-10 20:13:25 +0100

    ejercicios array 2 dimensiones terminados, pero mejorables

---
+++ programacion1DAM/src/array2dimensiones/Ejercicio3.java
@@ -0,0 +1,66 @@
+package array2dimensiones;
+
+public class Ejercicio3 {
+
+	public static void main(String[] args) {
+
+	
+		int matriz[][] = {
+				{5,10,15},
+				{20,25,30},
+				{35,40,45}
+				};
+System.out.print("Matriz original");
+		for (int i = 0; i < matriz.length; i++) {
+			System.out.print("\n");
+			for(int n = 0; n<matriz.length;n++) {
+				System.out.print(matriz[i][n] + " ");
+			}
+		}
+		
+		
+		/*Promedio en filas*/
+			System.out.println("\n");
+		System.out.println("\nPromedio en filas");
+		
+		 for (int i = 0; i < matriz.length; i++) {
+			 int suma = 0;
+				int sumatotal = 0;
+	            for (int j = 0; j < matriz.length; j++) {
+	            	suma += matriz[i][j];
+	                sumatotal++;
+	            }
+                double promedio = suma / sumatotal;
+       		 System.out.println(promedio);
+		 }
+		 
+		 /*Promedio en columnas*/
+			System.out.println("\nPromedio en columnas");
+			
+			 for (int i = 0; i < matriz.length; i++) {
+				 int suma = 0;
+					int sumatotal = 0;
+		            for (int j = 0; j < matriz.length; j++) {
+		            	suma += matriz[j][i];
+		                sumatotal++;
+		            }
+	                double promedio = suma / sumatotal;
+	       		 System.out.println(promedio);
+			 }
+		 
+		 
+
+			/*for (int i = 0; i < matriz.length; i++) {
+				System.out.print("\n");
+				for(int n = 0; n<matriz.length;n++) {
+					suma += matriz[i][n];
+					/*promedio = (matriz[i][n])/cantidad;
+					System.out.println(suma);
+					
+				}
+		
+	}*/
+
+
+}
+}
---
+++ programacion1DAM/src/array2dimensiones/Ejercicio4.java
@@ -0,0 +1,51 @@
+package array2dimensiones;
+import java.util.Scanner;
+public class Ejercicio4 {
+	public static void main(String[] args) {
+		
+		int veces=0;
+		int matriz[][] = {
+				{1,2,3},
+				{4,5,1},
+				{7,3,9}
+				};
+		System.out.print("Matriz original");
+		for (int i = 0; i < matriz.length; i++) {
+			System.out.print("\n");
+			for(int n = 0; n<matriz.length;n++) {
+				System.out.print(matriz[i][n] + " ");
+			}
+		}
+		
+		while(veces!=3) {
+		System.out.println("\ndime una ubicacion 0-2, 2 numeros");
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		Scanner sc1= new Scanner(System.in);
+		int i = sc1.nextInt();
+		System.out.println("dime otra ubicacion 0-2, 2 numeros");
+		Scanner sc2 = new Scanner(System.in);
+		int j = sc2.nextInt();
+		Scanner sc3= new Scanner(System.in);
+		int k = sc3.nextInt();
+		System.out.println("por ultimo dime otra ubicacion 0-2, 2 numeros");
+		Scanner sc4 = new Scanner(System.in);
+		int p = sc4.nextInt();
+		Scanner sc5= new Scanner(System.in);
+		int l = sc5.nextInt();
+		
+				while(n<=matriz.length && n>=0 && i<=matriz.length && i>=0 && j<=matriz.length && j>=0 && k<=matriz.length && k>=0 && p<=matriz.length && p>=0 && l<=matriz.length && l>=0) {
+					
+					veces++;
+					  int a = matriz[n][i];
+					  int b = matriz[j][k];
+					  int c = matriz[p][l];
+					  int sum = a + b + c;
+					  System.out.println("La suma de los numeros " + a + " + " + b + " + " + c + " es: " + sum);
+					  a = sc.nextInt();
+				
+				}System.out.println("Alguna posicion es invalida");
+
+	}
+	}
+}
